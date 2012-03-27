3f37fba06b67627245186cd7cd44bafac809bca5
hanv9488@gmail.com <hanv9488@gmail.com@88df74a2-ca2c-5d03-12ad-b26ed16e27d7>
2012-03-27 02:52:20 +0000

    menu left

--- trunk/code/views/home/_menu_left.php
+++ trunk/code/views/home/_menu_left.php
@@ -5,7 +5,7 @@
 	$cs->registerScriptFile('/static/treemenu/jquery.cookie.js');
 	$cs->registerCssFile('/css/jquery.treeview.css');
 
-
+	$currentId = $entry->id;
 	$ids[] = $entry->id;
 	$p = $entry->parent;
 	while (!empty($p)) {
@@ -14,36 +14,47 @@
 	}
  ?>
 <div class="main-left">
-    <div class="main-left-theo-doi">
-        <span><a href="#" class="main-left-theo-doi-a">LookupVietNam.vn đã tích hợp Bảo Kim</a></span>
-    </div>
+	<div class="main-left-theo-doi">
+		<span><a href="#" class="main-left-theo-doi-a">LookupVietNam.vn đã tích hợp Bảo Kim</a></span>
+	</div>
 	<div class="main-left-content">
 <ul id="navigation">
 	<?php foreach($categories as $category):?>
 		<li>
 			<a class=" <?php if(in_array($category->id,$ids)) echo 'active1';?>" href="<?php echo $this->createUrl('index',array('url_key'=>$category->url_key)) ?>"><?php echo $category->title;?></a>
-            <?php if(!empty($category->entries)): ?>
+			<?php if(!empty($category->entries)): ?>
 				<ul class="">
 				<?php foreach($category->entries as $entries):
 				?>
-                <li>
-                 	<a class=" <?php if(in_array($entries->id,$ids)) echo 'active1';?>" href="<?php echo $this->createUrl('index',array('url_key'=>$entries->url_key))?>"><?php echo $entries->title ?></a>
+				<li>
+					<a class=" <?php if(in_array($entries->id,$ids)) echo 'active1';?>" href="<?php echo $this->createUrl('index',array('url_key'=>$entries->url_key))?>"><?php echo $entries->title ?></a>
 
-                 	<?php  if(!empty($entries->entries)): ?>
+					<?php  if(!empty($entries->entries)): ?>
 						<ul class="">
 							<?php foreach($entries->entries as $entry_l3):
 							?>
 							<li class="">
-    							<a class=" <?php if(in_array($entry_l3->id,$ids)) echo 'active1';?>" href="<?php echo $this->createUrl('index',array('url_key'=>$entry_l3->url_key))?>"><?php echo $entry_l3->title ?></a>
+								<a class=" <?php if(in_array($entries->id,$ids)) echo 'active1';?>" href="<?php echo $this->createUrl('index',array('url_key'=>$entry_l3->url_key))?>"><?php echo $entry_l3->title ?></a>
+
+								<?php  if(!empty($entry_l3->entries)): ?>
+								<ul class="">
+									<?php foreach($entry_l3->entries as $entry_l4):
+									?>
+									<li class="">
+										<a class=" <?php if(in_array($entry_l4->id,$ids)) echo 'active1';?>" href="<?php echo $this->createUrl('index',array('url_key'=>$entry_l4->url_key))?>"><?php echo $entry_l4->title ?></a>
+									</li>
+									<?php endforeach;?>
+								</ul>
+							<?php endif;?>
 							</li>
-	                        <?php endforeach;?>
+							<?php endforeach;?>
 						</ul>
 					<?php endif;?>
 
 
-                </li>
-            	<?php endforeach; ?>
-                </ul>
+				</li>
+				<?php endforeach; ?>
+				</ul>
 			<?php endif; ?>
 		</li>
  	<?php endforeach; ?>
